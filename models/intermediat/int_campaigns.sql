a84fd95491ef3ae584bbcb79ed956ab26da143a7
OUBELLA Younes <younes.oubella.estc@gmail.com>
2025-09-30 13:33:46 +0000

    DAY2C2

---
+++ models/intermediat/int_campaigns.sql
@@ -0,0 +1,12 @@
+SELECT *
+ FROM {{ ref('stg_raw__adwords') }}
+ UNION ALL
+ SELECT *
+ FROM {{ ref('stg_raw__bing') }}
+ UNION ALL
+ SELECT *
+ FROM {{ ref('stg_raw__criteo') }}
+ UNION ALL
+ SELECT *
+ FROM {{ ref('stg_raw__facebook') }}
+ 
